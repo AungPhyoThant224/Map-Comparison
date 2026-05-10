ea16e4bc8ab4ad1503906347120f660eef676970
Aung Phyo Thant <aungphyothant224@gmail.com>
2026-05-10 17:53:20 +0630

    - Refactor : Remove unnecessary routes and its related files.

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,7 @@
 
 use App\Http\Controllers\MapController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware(['auth', 'verified'])->group(function () {
 
@@ -35,3 +20,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
